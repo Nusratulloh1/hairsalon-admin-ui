95f1ca5699b631320b59c13a8bc231f010f2eaf1
Nusratulloh1 <axunovnusrat@gmail.com>
2023-06-15 12:30:52 +0500

    responsive updates

--- src/views/landing/components/LHeader.vue
+++ src/views/landing/components/LHeader.vue
@@ -2,15 +2,15 @@
     <header class="top-section relative">
         <nav
             class="xl:container mx-auto px-5 sm:px-7 md:px-9 lg:px-12 xl:px-0 2xl:max-w-[100%] 2xl:mx-0 sm:pt-10 2xl:px-[72px]">
-            <ul class="flex items-start justify-between">
+            <ul class="flex sm:items-start justify-between">
                 <li>
                     <a href="/">
                         <AUIconWithName class=" w-28 h-28 sm:w-auto sm:h-auto"></AUIconWithName>
                     </a>
                 </li>
-                <li class="flex items-center gap-6">
+                <li class="flex items-center gap-3 sm:gap-6">
                     <a href="tel:+998 71 200-05-22"
-                        class="  hidden sm:flex items-center gap-1 sm:gap-[10px] font-semibold text-white text-xs sm:text-xl">
+                        class=" hidden md:flex items-center gap-1 sm:gap-[10px] font-semibold text-white text-xs sm:text-xl">
                         <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M19.456 14.4136C19.2889 15.6839 18.665 16.85 17.701 17.6939C16.7369 18.5379 15.4986 19.0021 14.2173 18.9998C6.77354 18.9998 0.717294 12.9436 0.717294 5.49981C0.714997 4.21852 1.17919 2.98021 2.02317 2.01615C2.86715 1.05209 4.0332 0.428223 5.30354 0.26106C5.62478 0.221836 5.95009 0.287556 6.23091 0.448409C6.51172 0.609262 6.73299 0.856621 6.86167 1.15356L8.84167 5.57387V5.58512C8.94019 5.81242 8.98088 6.06058 8.9601 6.30744C8.93932 6.5543 8.85773 6.79217 8.72261 6.99981C8.70573 7.02512 8.68792 7.04856 8.66917 7.072L6.71729 9.38575C7.41948 10.8126 8.91198 12.292 10.3576 12.9961L12.6395 11.0545C12.6619 11.0357 12.6854 11.0181 12.7098 11.002C12.9173 10.8636 13.1559 10.7792 13.4043 10.7562C13.6526 10.7333 13.9027 10.7727 14.132 10.8707L14.1442 10.8764L18.5607 12.8554C18.8582 12.9837 19.1062 13.2047 19.2675 13.4856C19.4289 13.7664 19.4951 14.092 19.456 14.4136Z"
@@ -44,7 +44,7 @@
                         </el-dropdown>
                     </div>
                     <router-link to="/login"
-                        class="bg-blur text-[#EBEBEB] text-xs sm:text-base font-semibold flex items-center gap-2 p-[6px] sm:p-[10px] rounded-xl w-[114px]">
+                        class="bg-blur text-[#EBEBEB] text-xs sm:text-base font-semibold flex items-center gap-2 p-[6px] sm:p-[10px] rounded-xl sm:w-[114px]">
                         <svg width="21" height="19" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M20.3672 18.6251C20.3013 18.7391 20.2066 18.8338 20.0926 18.8997C19.9785 18.9655 19.8492 19.0001 19.7175 19.0001H1.71749C1.58591 19 1.45667 18.9652 1.34276 18.8993C1.22885 18.8335 1.13429 18.7388 1.06855 18.6248C1.00282 18.5108 0.968228 18.3815 0.968262 18.2499C0.968296 18.1183 1.00295 17.9891 1.06874 17.8751C2.49656 15.4067 4.69687 13.6367 7.26468 12.7976C5.99452 12.0415 5.00769 10.8893 4.45573 9.51804C3.90377 8.14678 3.8172 6.63224 4.20931 5.20701C4.60143 3.78178 5.45054 2.52467 6.62627 1.62873C7.80199 0.732786 9.23931 0.247559 10.7175 0.247559C12.1957 0.247559 13.633 0.732786 14.8087 1.62873C15.9844 2.52467 16.8336 3.78178 17.2257 5.20701C17.6178 6.63224 17.5312 8.14678 16.9793 9.51804C16.4273 10.8893 15.4405 12.0415 14.1703 12.7976C16.7381 13.6367 18.9384 15.4067 20.3662 17.8751C20.4322 17.989 20.467 18.1183 20.4672 18.25C20.4674 18.3816 20.4329 18.511 20.3672 18.6251Z"
@@ -55,11 +55,13 @@
                 </li>
             </ul>
         </nav>
-        <div class="max-w-[770px] mx-auto mt-[6.5vh] text-center px-5 sm:px-0">
+        <div class="max-w-[980px] mx-auto mt-[6.5vh] text-center px-5 sm:px-0">
             <h2 class="font-semibold text-3xl sm:text-5xl  lg:text-[56px] lg:leading-[68px] text-white">
+                {{ $t('landing.dear') }}
+                <br class="mt-1">
                 {{ $t('landing.welcome') }}
-                <br>
-                <span class="text-[#FF0000]">{{ $t('landing.au') }}</span>
+                <span class="text-[#FF0000]">AKFA</span>
+                {{ $t('landing.au') }}
                 <br>
                 {{ $t('landing.welcome2') }}
             </h2>
@@ -124,4 +126,5 @@ const localeSet = (new_locale: string) => {
 .bg-blur {
     background: rgba(253, 253, 253, 0.4);
     backdrop-filter: blur(2px);
-}</style>
+}
+</style>
